dfccd9ba2eb737d061557769c7ea78f043362320
Allison Bartlett <allibartlett.27@gmail.com>
2021-03-15 21:26:57 -0500

    The correct Major EQ Data Map

--- Earthquake_Challenge/js/major_eq_starter_logic.js
+++ Earthquake_Challenge/js/major_eq_starter_logic.js
@@ -18,7 +18,6 @@ let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/sate
 let outdoors = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: "mapbox/outdoors-v11",
     accessToken: API_KEY
 });
 // Create the map object with center, zoom level and default layer.
